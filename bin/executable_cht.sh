c8068047ab66fdab350a87fc5770cffd540215a6
komori <973294+komori@users.noreply.github.com>
2023-01-04 13:55:35 -0600

    additional shell and vim config

---
+++ bin/executable_cht.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+## example of using arguments to a script
+#echo "My first name is $1"
+curl cheat.sh/"$1"
---
+++ bin/executable_winekill.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo killall -9 plugplay.exe
+sudo killall -9 explorer.exe
+sudo killall -9 services.exe
+sudo killall -9 winedevice.exe
+sudo killall -9 wineboot.exe
+
